1bca02ebcec0c5777b7d793c6f710e575c776750
quintonthompson <qwthompson1@gmail.com>
2020-09-12 23:28:21 -0500

    basic weather car

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -1,1 +1,17 @@
-import React from 'react'
+import React from 'react';
+
+const WeatherCard = (props) => {
+    return ( 
+        <div className="card">
+            <div className="location">
+                <h1 className="city" >Sydney</h1>
+                <h3 className="country" >AU</h3>
+            </div>
+            <img className="icon" src="./img/clouds.png" alt="WeatherIcon"/>
+            <h1 className="temp" >20 °C</h1>
+            <h3 className="condition" >Clouds</h3>
+        </div>
+     );
+}
+ 
+export default WeatherCard ;
